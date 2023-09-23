b48ea64317c165aac7df98b872c775e61b6d2bf4
Manjil Tamang <lamamanjil@gmail.com>
2023-09-23 20:03:52 +0545

    fix: WIP convert default cover image into base64

--- pages/api/now-playing.ts
+++ pages/api/now-playing.ts
@@ -34,6 +34,8 @@ export default async function handler(
     images[images.length - 1]?.url ||
     `${req.headers.host}/assets/default-artwork.jpg`;
 
+  console.log("Cover", `${req.headers.host}/assets/default-artwork.jpg`);
+
   let coverImg;
 
   try {
