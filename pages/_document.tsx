bd92af4c71ac0eaffbc3af0145c60d3569e7c794
Manjil Tamang <lamamanjil@gmail.com>
2023-09-24 22:26:11 +0545

    fix: move meta tags from _document to _app

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,15 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Spotify playing badge</title>
-        <meta
-          name="description"
-          content="Spotify playing badge - Manjil Tamang"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
